e2b5d397e4b18eb96a59d99ccd787262e6b024bc
John Andre Yap <johnandresyap510@gmail.com>
2025-09-26 23:47:38 +0800

    Added barebones linear regression code

---
+++ models/linear_regression.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class LinearRegression:
+    def __init__(self, fit_intercept=True):
+        self.fit_intercept = fit_intercept
+        self.coef_ = None
+        self.intercept_ = None
+
+    def fit(self, X, y):
+        return
+    
+    def predict(self, X, y):
+        return
+    
+    def score(self, X, y):
+        return
