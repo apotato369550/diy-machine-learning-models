f4fc60cadbd6a162179fc7380ec9cd2e427c1afd
John Andre Yap <johnandresyap510@gmail.com>
2025-10-01 23:55:53 +0800

    Did metrics and testing

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from utils import make_linear, train_test_split
+from utils import mse, rmse, mae, r2_score
 from models import SimpleLinearRegression
 
-X, y = make_linear(100, 7, 4.0, 6, 42069)
+X, y = make_linear(100, 3, 4.0, 6, 42069)
 
 # first 5 features/samples
 print(X[:5])
 
 # first 5 labels
 print(y[:5])
+
+# let's do "eda" next lol
 
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
@@ -31,3 +34,9 @@ r_squared = linreg.score(X_test, y_test)
 # figure out how to interpret this, then maybe confusion matrix probably
 # metrics.py
 print("r-squared score: " + str(r_squared))
+
+# scoringgg
+print("MSE: " + str(mse(y_test, y_pred)))
+print("RMSE: " + str(rmse(y_test, y_pred)))
+print("MAE: " + str(mae(y_test, y_pred)))
+print("R2 Score: " + str(r2_score(y_test, y_pred)))
