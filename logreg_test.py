8cfd5733c3195b839c3ccb1496941f6f6cae042c
jyap510 <johnandresyap510@gmail.com>
2025-10-10 22:43:14 +0800

    Started adding data visualization

--- logreg_test.py
+++ logreg_test.py
@@ -1,6 +1,7 @@
 from utils.data_gen import make_logistic
 from utils import train_test_split
 from models.simple_logistic_regression import SimpleLogisticRegression
+import matplotlib.pyplot as plt
 
 # create toy dataset
 X, y, coef, intercept = make_logistic(
@@ -17,8 +18,18 @@ print("True intercept: ", intercept)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
+plt.figure(figsize=(8, 6))
+plt.scatter(X_train, y_train, color="blue", alpha=0.6, label="Train")
+plt.scatter(X_test, y_test, color="green", alpha=0.6, label="Test")
+
+plt.title("Simple Linear Regression Fit")
+plt.xlabel("X feature")
+plt.ylabel("y target")
+plt.legend()
+plt.show()
+
 # initialize model and train
-logreg = SimpleLogisticRegression(learning_rate=0.1, n_iters=1000, verbose=True)
+logreg = SimpleLogisticRegression(learning_rate=0.1, n_iters=2000, verbose=True)
 logreg.fit(X_train, y_train)
 
 # evaluate accuracy
