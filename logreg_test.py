5af537457851dca4b087ac4eb9f73095a30c63f6
jyap510 <johnandresyap510@gmail.com>
2025-10-11 21:57:30 +0800

    Worked on visualizations

--- logreg_test.py
+++ logreg_test.py
@@ -4,6 +4,7 @@ from models.simple_logistic_regression import SimpleLogisticRegression
 import matplotlib.pyplot as plt
 
 # create toy dataset
+# realization: x isn't 1d haha. it has 2 features
 X, y, coef, intercept = make_logistic(
     n_samples=200,
     n_features=2,
@@ -18,19 +19,36 @@ print("True intercept: ", intercept)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-plt.figure(figsize=(8, 6))
-plt.scatter(X_train, y_train, color="blue", alpha=0.6, label="Train")
-plt.scatter(X_test, y_test, color="green", alpha=0.6, label="Test")
+# fix this: plot each feature in an xy plane, and color by class
 
-plt.title("Simple Linear Regression Fit")
-plt.xlabel("X feature")
-plt.ylabel("y target")
+# plot 1: first feature
+plt.figure(figsize=(8, 6))
+plt.scatter(X_train[:, 0], y_train, color="blue", alpha=0.6, label="Train")
+plt.scatter(X_test[:, 0], y_test, color="green", alpha=0.6, label="Test")
+
+plt.title("Simple Logistic Regression Dataset")
+plt.xlabel("Feature 1")
+plt.ylabel("Target (y)")
+plt.legend()
+plt.show()
+
+# plot 2: all features
+# issue: ok there's something wrong with this part
+plt.figure(figsize=(8, 6))
+plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap='coolwarm', alpha=0.6, label='Train')
+plt.scatter(X_test[:, 0], X_test[:, 1], c=y_train, cmap='coolwarm', alpha=0.6, label='Train')
+
+plt.title("Logistic Regression Dataset")
+plt.xlabel("Feature 1")
+plt.ylabel("Feature 2")
 plt.legend()
 plt.show()
 
 # initialize model and train
 logreg = SimpleLogisticRegression(learning_rate=0.1, n_iters=2000, verbose=True)
 logreg.fit(X_train, y_train)
+
+# make decision boundary chuchuchcu
 
 # evaluate accuracy
 accuracy = logreg.score(X_test, y_test)
