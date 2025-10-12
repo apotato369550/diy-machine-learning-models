c930d3883993d4654aaa363798c91df86fd20536
jyap510 <johnandresyap510@gmail.com>
2025-10-13 00:17:24 +0800

    Fixed second visualization and added a third

--- logreg_test.py
+++ logreg_test.py
@@ -2,6 +2,7 @@ from utils.data_gen import make_logistic
 from utils import train_test_split
 from models.simple_logistic_regression import SimpleLogisticRegression
 import matplotlib.pyplot as plt
+import numpy as np
 
 # create toy dataset
 # realization: x isn't 1d haha. it has 2 features
@@ -36,7 +37,9 @@ plt.show()
 # issue: ok there's something wrong with this part
 plt.figure(figsize=(8, 6))
 plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap='coolwarm', alpha=0.6, label='Train')
-plt.scatter(X_test[:, 0], X_test[:, 1], c=y_train, cmap='coolwarm', alpha=0.6, label='Train')
+
+# fix: replaces 'y_train' with 'y_test'. test shoudl be paired with test not train
+plt.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap='coolwarm', alpha=0.6, label='Test')
 
 plt.title("Logistic Regression Dataset")
 plt.xlabel("Feature 1")
@@ -49,6 +52,20 @@ logreg = SimpleLogisticRegression(learning_rate=0.1, n_iters=2000, verbose=True)
 logreg.fit(X_train, y_train)
 
 # make decision boundary chuchuchcu
+# when studying in-depth, figure out what this snippet does
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100), np.linspace(y_min, y_max, 100))
+
+Z = logreg.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+plt.contourf(xx, yy, Z, alpha=0.2, cmap='coolwarm')
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap='coolwarm', edgecolor='k')
+plt.title("Logistic Regression Decision Boundary")
+plt.xlabel("Feature 1")
+plt.ylabel("Feature 2")
+plt.show()
 
 # evaluate accuracy
 accuracy = logreg.score(X_test, y_test)
