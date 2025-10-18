a2cf95688dff52ec72232f978a36e9b378b08ca5
jyap510 <johnandresyap510@gmail.com>
2025-10-19 00:06:45 +0800

    Added some methods to decision tree classifier

--- models/decision_tree_classifier.py
+++ models/decision_tree_classifier.py
@@ -2,11 +2,15 @@ import numpy as np
 
 class SimpleDecisionTreeClassifier:
     def __init__(self, max_depth=5, min_sample_split=2):
-        return
-    
+        self.max_depth = max_depth
+        self.min_samples_split = min_sample_split
+        self.tree_ = None
+
     def _gini(self, y):
         # compute gini impurity
-        return
+        classes, counts = np.unique(y, return_counts=True)
+        prob = counts / counts.sum()
+        return 1 - np.sum(prob ** 2)
 
     def _best_split(self, X, y):
         # find the best feature and threshold to split on
@@ -19,10 +23,16 @@ class SimpleDecisionTreeClassifier:
         self.tree_ = self._build_tree(X, y)
 
     def _predict_one(self, x, node):
-        return
+        if 'leaf' in node:
+            return node['leaf']
+        if x[node['feature']] <= node['threshold']:
+            return self._predict_one(x, node['left'])
+        else:
+            return self._predict_one(x, node['right'])
     
     def predict(self, X):
-        return
+        return np.array([self._predict_one(x, self.tree_) for x in X])
     
     def score(self, X, y):
-        return
+        preds = self.Predict(X)
+        return np.mean(preds == y)
