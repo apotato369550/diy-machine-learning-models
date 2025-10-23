1d2149177201cf072336ea16982759d32f45ebe8
jyap510 <johnandresyap510@gmail.com>
2025-10-23 23:00:46 +0800

    Got started with KMeans model

---
+++ models/simple_kmeans_classifier.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class KMeans:
+    def __init__(self, n_clusters=3, n_iters=100, random_state=None):
+        self.n_clusters = n_clusters
+        self.n_iters = n_iters
+        self.random_state = random_state
+
+    def fit(self, X):
+        return
+    
+    def _assign_clusters(self, X):
+        return
+    
+    def predict(self, X):
+        return
