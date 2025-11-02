bf99d3a885948ac0ab5e1b342070aab478b72484
John Andre Yap <johnandresyap510@gmail.com>
2025-11-03 01:29:19 +0800

    Got started with Gaussian Native Bayes modelg

---
+++ models/simple_gaussian_native_bayes.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class SimpleGaussianNB:
+    def fit(self, X, y):
+        return
+    
+    def _pdf(self, class_idx, x):
+        return
+    
+    def _posterior(self, x):
+        return
+    
+    def predict(self, X):
+        return
