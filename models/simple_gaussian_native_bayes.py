b10a6c8f60ed644d92384d4938825c65216d8ce5
John Andre Yap <johnandresyap510@gmail.com>
2025-11-03 23:38:11 +0800

    Worked on native bayes fit

--- models/simple_gaussian_native_bayes.py
+++ models/simple_gaussian_native_bayes.py
@@ -2,10 +2,25 @@ import numpy as np
 
 class SimpleGaussianNB:
     def fit(self, X, y):
-        return
+        self.classes = np.unique(y)
+        n_features = X.shape[1]
+
+        self.means = np.zeros((len(self.classes), n_features))
+        self.vars = np.zeros((len(self.classes), n_features))
+        self.priors = np.zeros(len(self.classes))
+
+        for index, c in enumerate(self.classes):
+            X_c = X[y == c]
+            self.means[index, :] = X_c.mean(axis=0)
+            self.vars[index, :] = X_c.var(axis=0) + 1e-9
+            self.priors[index] = X_c.shape[0] / X.shape[0]
     
     def _pdf(self, class_idx, x):
-        return
+        mean = self.means[class_idx]
+        var = self.vars[class_idx]
+        numerator = np.exp(- (x - mean) ** 2 / (2 * var))
+        denominator = np.sqrt(2 * np.pi * var)
+        return numerator / denominator
     
     def _posterior(self, x):
         return
