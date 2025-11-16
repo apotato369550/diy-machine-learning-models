3a3d36579846ef612e4ff3cdca8b756c6850d507
John Andre Yap <johnandresyap510@gmail.com>
2025-11-17 00:33:01 +0800

    Made simple KNN class template

---
+++ models/simple_knn.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class SimpleKNN:
+    def __init__(self, k=3):
+        return
+    
+    def fit(self, X, y):
+        return
+    
+    def _euclidean_distance(self, a, b):
+        return
+    
+    def _predict_one(self, x):
+        return
+    
+    def predict(self, X):
+        return
